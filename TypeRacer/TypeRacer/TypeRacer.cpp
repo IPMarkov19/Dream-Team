166ae4cb9bf692fdca787fafc5f8e065cf0dbe6e
HHKimryanov19 <HHKimryanov19@codingburgas.bg>
2021-02-24 20:20:51 +0200

    Add fundamentals of TypeRacer

--- TypeRacer/TypeRacer/TypeRacer.cpp
+++ TypeRacer/TypeRacer/TypeRacer.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 void border()
 {
+	cout << "              ---------------------------------------------------------------------" << endl;
 	cout << "               ____       ____     ___________     ____        __     __         __" << endl;
 	cout << "              |" << "    \\     /    |   |    _______|   |     \\     |  |   |  |       |  |" << endl;
 
@@ -14,11 +14,40 @@ void border()
 
 	cout << "              |" << "   |       |   |   |   |_______    |  |    \\  \\|  |   |  \\_______/  |" << endl;
 	cout << "              |" << "___|       |___|   |___________|   |__|     \\_____|    \\___________/" << endl;
-
+	cout << "              -----------------------------------------------------------------------" << endl;
 }
 int main()
 {
-	cout << "              ---------------------------------------------------------------------" << endl;
+	
 	border();
-	cout << "              -----------------------------------------------------------------------" << endl;
+    size_t length, wordsCounter = 0, startIndex = 0;
+    string words[1000];
+
+
+
+    string YourText;
+    string Text1 = "An essay is, generally, a piece of writing that gives the author's own argument, but the definition is vague, overlapping with those of a letter, a paper, an article, a pamphlet, and a short story. Essays have traditionally been sub-classified as formal and informal.";
+    length = Text1.size();
+    string RandomText;
+
+
+
+    for (size_t i = 0; i < length; i++)
+    {
+        if (Text1[i] == ' ')
+        {
+            words[wordsCounter] = Text1.substr(startIndex, i - startIndex);
+            startIndex = i + 1;
+            wordsCounter++;
+        }
+        else
+        {
+            if (i == length - 1)
+            {
+                words[wordsCounter] = Text1.substr(startIndex, length - startIndex);
+                startIndex = i + 1;
+                wordsCounter++;
+            }
+        }
+    }
 }
